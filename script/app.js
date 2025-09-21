ddeae87044b93716dfbc8aa8d78e330ca01ef4cd
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2025-09-21 18:26:38 -0300

    Enviando atualizacao ao github

--- script/app.js
+++ script/app.js
@@ -1,32 +1,39 @@
 let numeroSecreto;
-let tentativas = 0;
+let tentativas;
 console.log("Boas vindas ao jogo do número secreto");
 document.getElementById('iniciarJogo').addEventListener('click', iniciarJogo);
 
 function iniciarJogo() {
-  numeroSecreto = parseInt(Math.random() * 10) + 1; // 1 a 10
-  tentativas = 0;
-  console.log("Número secreto gerado:", numeroSecreto);
-  jogar();
-}
-
-function jogar() {
-  let chute = prompt("Escolha um número entre 1 e 10");
-  if (chute === null) return; 
-  chute = parseInt(chute);
-  tentativas++;
-  console.log("Chute do usuário:", chute, "Tentativa:", tentativas);
-
-  if (chute == numeroSecreto) {
-    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);
-    console.log("Acertou!");
-  } else {
-    if (chute > numeroSecreto) {
-      console.log("O número secreto é menor");
-    } else {
-      console.log("O número secreto é maior");
-    }
+    numeroSecreto = parseInt(Math.random() * 10) + 1;
+    tentativas = 0;
+    console.log("Novo jogo iniciado. Número secreto:", numeroSecreto); 
+    
     jogar();
-  }
+}
+function jogar() {
+    let chute = prompt("Escolha um número entre 1 e 10");
+    if (chute === null) {
+        alert("Jogo cancelado.");
+        return; 
+    }
+
+    chute = parseInt(chute);
+    tentativas++;
+    console.log("Chute do usuário:", chute, "| Tentativa nº:", tentativas);
+
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}!`);
+        console.log("O usuário acertou!");
+    
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}. Tente novamente!`);
+            console.log("O número secreto é menor");
+        } else {
+            alert(`O número secreto é maior que ${chute}. Tente novamente!`);
+            console.log("O número secreto é maior");
+        }
+        jogar();
+    }
 }
